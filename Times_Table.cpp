60b5f76553b46db135d67adda054b04986919955
swkiim <swkiim1229@gmail.com>
2022-03-18 15:27:56 +0900

    구구단 구현

---
+++ Times_Table.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int n;
+    int i = 1;
+
+    printf("Select Times Tabel : ");
+    scanf("%d", &n);
+
+    while (i <= 9){
+        printf("%d * %d = %d\n", n, i, n * i);
+        i++;
+    }
+    return 0;
+}
